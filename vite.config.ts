e33a83c774990826b3f6a5d4586ea56c890d1eed
rish0018 <rishit.23bce8103@vitapstudent.ac.in>
2026-05-11 00:47:43 +0530

    fixing errors still

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import path from "path";
 export default defineConfig({
   plugins: [
     tanstackRouter({
-      target: "react-spa",
+      target: "react",
       autoCodeSplitting: true,
     }),
     react(),
